68c81b477002c42c646816a8c570103853fd41db
Kosimkhuja Sharipov <sharipovqosim007@gmail.com>
2022-09-22 22:21:58 +0500

    A little change

--- app/src/main/java/com/example/lesson54/core/network/MovieAPI.kt
+++ app/src/main/java/com/example/lesson54/core/network/MovieAPI.kt
@@ -11,27 +11,27 @@ import retrofit2.http.Query
 
 interface MovieAPI {
 
-    @GET("3/movie/popular?api_key=ae228a09fd0c71679dabcf913aea5d11&language=ru-RU")
+    @GET("3/movie/popular?api_key=ae228a09fd0c71679dabcf913aea5d11&language=en-EN")
     fun popularMovies(
         @Query("page") page: String
     ): Single<PopularMovieResponse>
 
-    @GET("3/movie/top_rated?api_key=ae228a09fd0c71679dabcf913aea5d11&language=ru-RU")
+    @GET("3/movie/top_rated?api_key=ae228a09fd0c71679dabcf913aea5d11&language=en-EN")
     fun topRatedMovies(
         @Query("page") page: String
     ): Single<TopRatedMovieResponse>
 
-    @GET("3/movie/now_playing?api_key=ae228a09fd0c71679dabcf913aea5d11&language=ru-RU")
+    @GET("3/movie/now_playing?api_key=ae228a09fd0c71679dabcf913aea5d11&language=en-EN")
     fun nowPlayingMovies(
         @Query("page") page: String
     ): Single<NowPlayingMovieResponse>
 
-    @GET("3/movie/upcoming?api_key=ae228a09fd0c71679dabcf913aea5d11&language=ru-RU")
+    @GET("3/movie/upcoming?api_key=ae228a09fd0c71679dabcf913aea5d11&language=en-EN")
     fun upcomingMovies(
         @Query("page") page: String
     ): Single<UpcomingMovieResponse>
 
-    @GET("3/genre/movie/list?api_key=ae228a09fd0c71679dabcf913aea5d11&language=ru-RU")
+    @GET("3/genre/movie/list?api_key=ae228a09fd0c71679dabcf913aea5d11&language=en-EN")
     fun genres(): Single<MovieGenreResponse>
 
 }
